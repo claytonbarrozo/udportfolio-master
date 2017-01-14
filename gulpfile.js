8b1710e3e8a7eb052dfce5f9265279c2657b676f
Clayton de Oliveira <clayton.barrozo@gmail.com>
2017-01-14 19:55:44 +0000

    uglify the js page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
+	var gulp = require('gulp'), 
+		urglify = require ('gulp-uglify');
 
 gulp.task('default', function() {
   // place code for your default task here
+  gulp.src('js/*')
+  .pipe(urglify())
+  .pipe(gulp.dest('minjs'))
 });
