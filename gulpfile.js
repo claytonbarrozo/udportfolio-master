55099f19f7b1c23aa9839ae6c395aa7141b9e27b
Clayton de Oliveira <clayton.barrozo@gmail.com>
2017-01-14 22:42:35 +0000

    Css
    run gulp for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
-var gulp = require ('gulp'),
-	imagemin = require ('gulp-imagemin');
 
-// Iamge Task
-// Compress
-gulp.task ('image',function() {
-gulp.src('views/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('views/images'));
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+ 
+gulp.task('default', function() {
+  return gulp.src('views/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
 });
